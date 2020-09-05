0e589aac8d26203a3606b75bca47cf3651692dd7
kota_koyama <kouta103177@gmail.com>
2020-09-05 19:50:56 +0900

    リファクタリングを実行（formテンプレ変更,Before_action,戻るボタン）

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article,only: [:show, :edit, :update]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -12,7 +13,6 @@ class ArticlesController < ApplicationController
   end
 
   def  create
-    @article = Article.new(article_params)
     if @article.save
       redirect_to article_path(@article),notice: '保存できました'
     else
@@ -22,7 +22,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
@@ -40,13 +39,15 @@ class ArticlesController < ApplicationController
     article.destroy!
     redirect_to root_path,notice: '削除しました'
   end
-  
-
-  
 
   private
   def article_params
     params.require(:article).permit(:title,:content)
   end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  
 
 end
