c65be9a1b2d19d5142357314557a239d5b7b5a84
kota_koyama <kouta103177@gmail.com>
2020-09-06 18:39:55 +0900

    ユーザーと記事の紐付け

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,15 +7,11 @@
 #  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  user_id    :bigint
 #
-# Indexes
-#
-#  index_articles_on_user_id  (user_id)
-#
+
+
 class Article < ApplicationRecord
   validates :title, presence:true
   validates :content,presence:true
 
-  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,6 @@ ActiveRecord::Schema.define(version: 2020_09_05_111536) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
