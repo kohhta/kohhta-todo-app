372dcea143f0d01dfc0fdba625d9dbc4964e33ba
kota_koyama <kouta103177@gmail.com>
2020-09-07 18:28:44 +0900

    Userの紐付け

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,12 @@
 class Article < ApplicationRecord
   validates :title, presence:true
   validates :content,presence:true
+
+  belongs_to :user
+
+  def author_name
+    user.display_name
+  end
+  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_06_124000) do
+ActiveRecord::Schema.define(version: 2020_09_03_121324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
