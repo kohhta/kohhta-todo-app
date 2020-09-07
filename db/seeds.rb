afd8a1b8b02c3329c3a92d5b0dd2881a59c5c097
kota_koyama <kouta103177@gmail.com>
2020-09-07 18:40:33 +0900

    ユーザー紐付け

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# jon = User.create!(email: 'john@example.com', password: 'password')
-# emily = User.create!(email: 'emily@example.com', password: 'password')
+jon = User.create!(email: 'john@example.com', password: 'password')
+emily = User.create!(email: 'emily@example.com', password: 'password')
 
 
 Article.create({title: '新しくすること', content: 'オフィスデスクの掃除をする'})
