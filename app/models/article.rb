12fac96ed9b41ea3f64c15d000261ec9ae184e9e
kota_koyama <kouta103177@gmail.com>
2020-09-07 18:39:39 +0900

    ユーザーの紐付け

--- app/models/article.rb
+++ app/models/article.rb
@@ -19,4 +19,10 @@ class Article < ApplicationRecord
   validates :title, presence:true
   validates :content,presence:true
 
+  belongs_to :user
+
+  def author_name
+    user.display_name
+  end
+  
 end
