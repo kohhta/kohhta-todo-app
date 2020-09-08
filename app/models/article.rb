cf0c3256a055bce448cfe93a500705a084da11a7
kota_koyama <kouta103177@gmail.com>
2020-09-08 23:27:40 +0900

    アイキャッチ実装、ロゴでroot_path戻る

--- app/models/article.rb
+++ app/models/article.rb
@@ -16,6 +16,8 @@
 
 
 class Article < ApplicationRecord
+  has_one_attached :eyecatch
+
   validates :title, presence:true
   validates :content,presence:true
   has_many :comments, dependent: :destroy
